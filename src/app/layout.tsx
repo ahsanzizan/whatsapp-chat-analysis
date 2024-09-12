f79117247b304fff99ec6a15ff9e2bc89a5d6c45
Ahsan <ahsanaz461@gmail.com>
2024-09-12 14:23:20 +0700

    feat: Add basic SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
+import { Metadata } from "next";
 import "./globals.css";
 import { Roboto_Mono } from "next/font/google";
 
 const robotoMono = Roboto_Mono({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "WhatsApp Chat Analyzer by Ahsan Azizan",
+  description:
+    "A WhatsApp-exported .txt file analyzer with a couple of analysis.",
+};
 
 export default function RootLayout({
   children,
